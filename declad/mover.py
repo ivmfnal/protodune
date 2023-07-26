a14f4c352688de222db2e5daeb43a6f939ccd85a
Igor Mandrichenko <igorvm@gmail.com>
2023-07-26 14:31:08 -0500

    Ignore both "user" and "creator" in the input metadata when converting to MetaCat Ignore erorrs making dirs - assume it's complaining that the dir already exists. If not, the subsequent transfer will fail anyway

--- declad/mover.py
+++ declad/mover.py
@@ -71,6 +71,7 @@ class MoverTask(Task, Logged):
         metadata.pop("checksum", None)
         metadata.pop("file_name", None)
         metadata.pop("creator", None)           # ignored
+        metadata.pop("user", None)              # ignored
 
         out = {}
         #
@@ -294,7 +295,7 @@ class MoverTask(Task, Logged):
 
             ret, output = runCommand(create_dirs_command, self.TransferTimeout, self.debug)
             if ret:
-                return self.failed("Create dirs failed: %s" % (output,))
+                return self.debug("create dirs failed (will be ignored assuming it already exists): %s" % (output,))
 
             copy_cmd = self.Config["copy_command_template"] \
                 .replace("$dst_url", data_dst_url)  \
